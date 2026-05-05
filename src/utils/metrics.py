a0528b08438c7def8515ba436e0a39ca81ba4c70
lastvallian <lastvallian1017@gmail.com>
2026-05-05 16:53:24 -0400

    Fixed score of denominator is 0

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -4,6 +4,7 @@
 # In[ ]:
 
 
+import numpy as np
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc
 
 def evaluate_model(y_test, y_pred, y_proba=None):
@@ -12,6 +13,7 @@ def evaluate_model(y_test, y_pred, y_proba=None):
     results["confusion_matrix"] = confusion_matrix(y_test, y_pred)
 
     if y_proba is not None and len(y_proba.shape) == 2:
+        y_proba = np.nan_to_num(y_proba, nan=0.0, posinf=1.0, neginf=0.0)
         fpr, tpr, _ = roc_curve(y_test, y_proba[:, 1])
         results["roc"] = {
             "fpr": fpr,
